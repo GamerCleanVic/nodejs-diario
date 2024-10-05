dbaa4143dda4d3b86c0d0a5b76db9a31db5574dc
GamerCleanVic <jonassd2000@gmail.com>
2024-10-05 18:17:17 -0300

    env file

--- src/teste.ts
+++ src/teste.ts
@@ -1,6 +1,8 @@
 //Modulo 2: #10 - Uso de variáveis de ambiente
 
 let nomeTeste: string = "João";
-let idadeTeste: number = 30;
+let idadeTeste: number = 40;
+let token: string = process.env.GOOGLE_DRIVE_TOKEN as string;
 
 console.log(`Oi meu nome é ${nomeTeste} e tenho ${idadeTeste}.`);
+console.log(`Conectando no Drive com o token: ${token}`);
